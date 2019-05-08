7a49776d01f8bf70c2229528d0965736ece23a91
Maxi Vadora <maxivadora@gmail.com>
2019-05-08 11:38:01 -0300

    added jwt

--- app/controllers/concerns/exception_handler.rb
+++ app/controllers/concerns/exception_handler.rb
@@ -1,13 +1,21 @@
 module ExceptionHandler
   extend ActiveSupport::Concern
 
+  class InvalidToken < StandardError; end
+
   included do
     # ActiveRecord
     rescue_from ActiveRecord::RecordInvalid, with: :error_422
     rescue_from ActiveRecord::RecordNotFound, with: :error_404
+    # Custom
+    rescue_from ExceptionHandler::InvalidToken, with: :error_401
   end
 
   private
+  
+  def error_401 e
+    render json: { message: e.message }, status: :unauthorized
+  end
 
   def error_404 e
     render json: { message: e.message }, status: :not_found
---
+++ app/lib/json_web_token.rb
@@ -0,0 +1,16 @@
+class JsonWebToken
+  HMAC_SECRET = Rails.application.credentials.secret_key_base
+
+  def self.encode(payload, exp = 24.hours.from_now)
+    payload[:exp] = exp.to_i
+    JWT.encode(payload, SECRET_KEY)
+  end
+
+  def self.decode(token)
+    body = JWT.decode(token, HMAC_SECRET)[0]
+    HashWithIndifferentAccess.new body
+    # rescue from all decode errors
+  rescue JWT::DecodeError => e
+    raise ExceptionHandler::InvalidToken, e.message
+  end
+end
